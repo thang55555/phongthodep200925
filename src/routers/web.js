a4920ebba410a94cf70532b99fcb5c59920b0bf0
thang55555 <bsthang9x@gmail.com>
2026-07-23 23:51:11 +0700

    23.07.26

--- src/routers/web.js
+++ src/routers/web.js
@@ -208,6 +208,7 @@ router.get("/search", SiteController.search);
 router.get("/view-san-pham/:slug/:id", SiteController.productsp);
 router.get("/view-dich-vu/:slug/:id", SiteController.productdichvu);
 router.get("/view-tin-tuc/:slug/:id", SiteController.productTinTuc);
+router.get("/view-category-dich-vu/:slug/:id", SiteController.categorydichvu);
  
  
 module.exports = router;
